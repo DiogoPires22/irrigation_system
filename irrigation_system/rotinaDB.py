e05710db40696ae7a84a0d9af3c7724fbb74abdb
diogopires22 <diogopires.vr@hotmail.com>
2014-12-08 02:28:49 +0000

    area de controle do sistema concluida

--- irrigation_system/rotinaDB.py
+++ irrigation_system/rotinaDB.py
@@ -1,12 +1,14 @@
 import serial,sqlite3, datetime, json,sys, os
 
 #atribui a conexao serial
-ser = serial.Serial('/dev/ttyACM0',9600)
+aberto=True
+ser=serial.Serial('/dev/ttyACM0',9600)
 
 
 import numbers
 
 while 1:
+  
     try:
 		#abre uma conexao com o banco da aplicacao
 		conn=sqlite3.connect('db.sqlite3')
@@ -19,16 +21,19 @@ while 1:
 		status=rows[0][0]
 		#se estiver ligado(status=1) faremos a comunicacao serial, e iremos inserir uma tupla com o valor do sensor e o horario da medicao
 		if status==1:
+			ser.open()
+			aberto=True
 			#informa o arduino q o sistema esta ligado
 			ser.write('1')
 			
 			#atribui a variavel o retorno do arduino
-			moisture=ser.readline()	
+			moisture=ser.readline()
+
 			#converte o retorno em um json object
 			json_object=json.loads(moisture)
 			#informa o valor do sensor no terminal
+			                 
 			print(json_object["moisture"])
-			
 			#faz a insercao do valor no banco
 			c.execute('INSERT INTO irrigation_soilmoisturecontrol (moisture,date) vALUES (?,?)',(json_object["moisture"],datetime.datetime.now()))
 			#comita a insercao
@@ -37,11 +42,14 @@ while 1:
 			conn.close()
 		else:
 			#se estiver com status 0 informamos ao arduino q o sistema esta desativado
-			ser.write('0')
-			
+			if aberto:
+				ser.close()
+				aberto=False	
 			print("sistema desligado")
+		
     except Exception as ex:
 		exc_type,exc_value,exc_traceback=sys.exc_info()
-		print ex
-   
+		print(ex)
+		print exc_traceback.tb_lineno
+	
     #time.sleep(5)
